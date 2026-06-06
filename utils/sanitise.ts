724bd4a1d76d2b0583519772f1d87833d8aad28f
Team Firefli <team@firefli.com>
2026-06-06 11:53:17 +0000

    Fix - Sanitisation Vuln

--- utils/sanitise.ts
+++ utils/sanitise.ts
@@ -20,6 +20,13 @@ export function sanitizeJSON(node: any): any {
       const attrs: any = { ...out.attrs };
       if (attrs.src && !isSafeUrl(attrs.src)) delete attrs.src;
       if (attrs.href && !isSafeUrl(attrs.href)) delete attrs.href;
+      if (attrs.width !== undefined && !/^\d+(\.\d+)?(%|px|em|rem)?$/.test(String(attrs.width))) {
+        delete attrs.width;
+      }
+      const SAFE_ALIGNMENTS = new Set(["left", "right", "center", "justify"]);
+      if (attrs.textAlign !== undefined && !SAFE_ALIGNMENTS.has(String(attrs.textAlign))) {
+        delete attrs.textAlign;
+      }
       out.attrs = attrs;
     }
 
